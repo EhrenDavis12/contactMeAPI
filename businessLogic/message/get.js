149675eab0ebbb485661d4d5aca2bb4e59339cf8
Ehren Davis <ehren.davis12@gmail.com>
2019-04-07 17:13:38 -0700

    working whats app integration

--- businessLogic/message/get.js
+++ businessLogic/message/get.js
@@ -3,20 +3,6 @@ const db = require("../../models");
 module.exports = function(app) {
   require("../util/errorHelpers");
 
-  this.getOneMessageOrg = (query, res) => {
-    db.message
-      .findOne({
-        where: query
-      })
-      .then(function(result) {
-        res.json(result);
-      })
-      .catch(function(err) {
-        console.log(err.message);
-        res.status(400).json(err.message);
-      });
-  };
-
   this.getMessageByUuid = async uuid => {
     if (!uuid) throwError(400, "invalid request", "No UUID provided")();
 
